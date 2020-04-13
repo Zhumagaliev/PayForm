52dc1645766b85cad2402af0fa188f76f9729257
Zhumagaliev12 <61092517+Zhumagaliev12@users.noreply.github.com>
2020-04-13 23:28:49 +0600

    first commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,31 +8,81 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            // необобщенная коллекция ArrayList
-            ArrayList objectList = new ArrayList() { 1, 2, "string", 'c', 2.0f };
-
-            object obj = 45.8;
-            objectList.Add(obj);
-            
-            objectList.Add("string2");//Метод добавления новых элементов.
-            objectList.RemoveAt(0); // удаление первого элемента
-            foreach (object o in objectList)
-            {
-                Console.WriteLine(o);
-            }
-            Console.WriteLine("Общее число элементов коллекции: {0}", objectList.Count);//Свойство только для чтения для получения общего количества элементов.
+           string number = Console.ReadLine();
+            int number2 = Convert.ToInt32(Console.ReadLine());
+           Listtt<string> p = new Listtt<string>();
+           p.Add(number);
             
             
-            // обобщенная коллекция List
-            List<string> countries = new List<string>() { "Казахстан", "Россия", "Украина", "Азербайджан" };
-            countries.Add("США");
-            countries.RemoveAt(1); // удаление второго элемента
-            foreach (string s in countries)
-            {
-                Console.WriteLine(s);
-            }
-
+            Console.WriteLine(p[number2]);
+            
+            //p.RemoveLast();
+            Console.WriteLine(p.Length);
             Console.ReadLine();
         }
+
+        class Listtt<B>
+        {
+
+            private B[] _array = new B[10];
+            private B[] _tarray = new B[10];
+            private int count = 0;
+            public int Length { get; private set; }
+            public B this[int index]
+            {
+
+                get
+                {
+                    if (index < count)
+                        return _array[index];
+                    else return _array[0];
+
+                }
+                //set {
+                // if (index < _array.Length)
+                // _array[index] = value;
+                // else {
+                // newArray();
+                // _array[index] = value;
+                // }
+                //}
+            }
+
+            //public void Add(B v) {
+            // if(_array[_array.Length-1] != null)
+            //}
+
+            public void Add(B i)
+            {
+                count++;
+                if (_array.Length == count) newArray();
+                _array[count - 1] = i;
+                Length = count;
+            }
+
+            public void RemoveLast()
+            {
+                if ((float)_array.Length * 0.75 >= count - 1)
+                {
+                    Array.Copy(_array, 0, _tarray, 0, count - 1);
+                    _array = new B[(int)(_array.Length * 0.75)];
+                    Array.Copy(_tarray, 0, _array, 0, count - 1);
+                    count--;
+                    //обнуление массива таррей
+                }
+            }
+
+            public void error() { Console.WriteLine("Error"); }
+            private void newArray()
+            {
+                Array.Copy(_array, 0, _tarray, 0, _array.Length);
+                _array = new B[_array.Length * 2];
+                Array.Copy(_tarray, 0, _array, 0, _tarray.Length);
+                //обнуление массива таррей
+            }
+
+        }
+
+        
     }
 }
